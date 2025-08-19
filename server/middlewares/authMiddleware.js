e4cfd08ed0493131c9dbd9ad584f1e0c40ce580e
Anish Jaiswal <anishjaiswal1220@gmail.com>
2025-08-19 12:44:46 +0530

    added whatsapp notificaion and .env.example

--- server/middlewares/authMiddleware.js
+++ server/middlewares/authMiddleware.js
@@ -38,6 +38,9 @@ exports.protect = async (req, res, next) => {
 // ADD THIS FUNCTION TO YOUR FILE:
 exports.authorize = (...roles) => {
   return (req, res, next) => {
+    if(req.user.role === "super-admin"){
+      return next();
+    }
     if (!roles.includes(req.user.role)) {
       return res.status(403).json({
         message: `User role '${req.user.role}' is not authorized`,
