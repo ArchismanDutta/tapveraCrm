ee76a17200faa7cd67714429699e23ccb7b8e337
Archisman Dutta <archismandutt3@gmail.com>
2025-08-22 16:58:15 +0530

    added new things

--- server/routes/leaveRoutes.js
+++ server/routes/leaveRoutes.js
@@ -8,8 +8,15 @@ const { protect, authorize } = require("../middlewares/authMiddleware");
 router.post("/", protect, authorize("employee"), leaveController.createLeave);
 router.get("/mine", protect, authorize("employee"), leaveController.getUserLeaves);
 
+// Team leaves route (now accessible to employees too)
+router.get(
+  "/team",
+  protect,
+  authorize("employee", "admin", "super-admin"), // allow employee to view their department leaves
+  leaveController.getTeamLeaves
+);
+
 // Admin & Super-admin routes
-router.get("/team", protect, authorize("admin", "super-admin"), leaveController.getTeamLeaves); // fetch team leaves
 router.get("/", protect, authorize("admin", "super-admin"), leaveController.getAllLeaves);
 router.patch("/:id", protect, authorize("admin", "super-admin"), leaveController.updateLeaveStatus);
 
