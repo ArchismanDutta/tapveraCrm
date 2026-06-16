ad4915d3d6ce5f24c9edd7bc77dcbeff595a9100
anish2210 <anishjaiswal1220@gmail.com>
2026-06-16 10:27:52 +0530

    feat(ios): add webkit scrolling styles for smooth iOS scrolling

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,5 +1,6 @@
 import { createRoot } from "react-dom/client";
 import "./index.css";
+import "./styles/ios-scrolling.css";
 import App from "./App.jsx";
 import { Provider } from "react-redux";
 import store from "./store/index.js";
